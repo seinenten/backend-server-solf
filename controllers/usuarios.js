01c47454c2ac44d25fd8112cc0ea84f92d79f13e
jorge <20200707@uthh.edu.mx>
2023-02-26 08:13:05 -0600

    a
    a

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -113,7 +113,15 @@ const actualizarUsuario = async( req, res = response ) => {
             }
         }
 
-        campos.email = email;
+        //Si se intenta modificar el email de las cuentas de google registradas
+        if( !usuarioDB.google ){
+            campos.email = email;
+        } else if (usuarioDB.email !== email){
+            return res.status(400).json({
+                ok: false,
+                msg: 'Usuario de google no pueden cambiar su correo'
+            })
+        }
 
         const usuarioActualizado = await Usuario.findByIdAndUpdate( uid, campos, { new: true } );
 
