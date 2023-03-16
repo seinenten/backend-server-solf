4b4be7dcb3bdf3af53d07505c5e28c3e016b0c69
jorge <20200707@uthh.edu.mx>
2023-03-16 00:49:30 -0600

    a
    a

--- helpers/google-verify.js
+++ helpers/google-verify.js
@@ -10,7 +10,7 @@ async function googleverify(token ) {
       //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
   });
   const payload = ticket.getPayload();
-  console.log({payload})
+  // console.log({payload})
 
   // If request specified a G Suite domain:
   
