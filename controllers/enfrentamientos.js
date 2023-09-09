f09a7cdccc3050472ce0013852d0b8afb5d629f3
jorge <20200707@uthh.edu.mx>
2023-09-09 10:39:36 -0600

    jornadas
    jornadas

--- controllers/enfrentamientos.js
+++ controllers/enfrentamientos.js
@@ -12,48 +12,51 @@ const generarEnfrentamientosPorLiga = async(req, res = response) => {
 
     const totalEquipos = equipos.length;
 
-    if (totalEquipos % 2 === 0) {
-        // El conteo de equipos es un número par, podemos dividirlos en dos arreglos
-        const mitad = totalEquipos / 2;
+    const jornadas = [];
+    const enfrentamientos = [];
 
-        // Divide el array de equipos en dos arreglos
-        const equipoVisitante = equipos.slice(0, mitad);
-        const equipoLocal = equipos.slice(mitad);
+    // Calcular el número de jornadas (totalEquipos si es impar, totalEquipos - 1 si es par)
+    const numeroJornadas = totalEquipos % 2 === 0 ? totalEquipos - 1 : totalEquipos;
 
-         // Crear jornadas (totalEquipos - 1 jornadas)
-        for (let i = 1; i <= totalEquipos - 1; i++) {
-            const jornada = new Jornada({
-                nombre: `jornada ${i}`,
-            });
-            await jornada.save();
-
-            // Crear enfrentamientos para la jornada
-            for (let j = 0; j < mitad; j++) {
-                const enfrentamiento = new Enfrentamiento({
-                    jornada: jornada._id,
-                    equipoVisitante: equipoVisitante[j]._id,
-                    equipoLocal: equipoLocal[j]._id,
-                });
-                await enfrentamiento.save();
-            }
-
-            // Rotar los grupos para la próxima jornada
-            equipoVisitante.push(equipoLocal.shift());
-            equipoLocal.push(equipoVisitante.pop());
-
-
-        }
-
-        // Hacer lo que necesites con los dos grupos de equipos
-        // Por ejemplo, puedes enviarlos como respuesta en JSON
-        res.status(200).json({
-            equipoVisitante,
-            equipoLocal
-        });
-    } else {
-        // El conteo de equipos es impar, maneja esta situación según tus necesidades
-        res.status(400).json({ mensaje: "El número de equipos no es par." });
+    // Crear jornadas de acuerdo al número de jornadas calculado
+    for (let i = 1; i <= numeroJornadas; i++) {
+        jornadas.push(new Jornada({
+        nombre: `Jornada ${i}`,
+        }));
     }
+
+    // Crear una matriz de enfrentamientos vacía
+    for (let i = 0; i < totalEquipos; i++) {
+        enfrentamientos.push([]);
+    }
+
+    // Generar los enfrentamientos de acuerdo a las restricciones
+    for (let i = 0; i < numeroJornadas; i++) {
+        for (let j = 0; j < totalEquipos / 2; j++) {
+        const equipoLocalIndex = j;
+        const equipoVisitanteIndex = (totalEquipos - 1 - j + i) % (totalEquipos - 1);
+        
+        const jornada = jornadas[i];
+        const equipoLocal = equipos[equipoLocalIndex];
+        const equipoVisitante = equipos[equipoVisitanteIndex];
+
+        enfrentamientos[equipoLocalIndex].push(equipoVisitanteIndex);
+        enfrentamientos[equipoVisitanteIndex].push(equipoLocalIndex);
+
+        const enfrentamiento = new Enfrentamiento({
+            jornada: jornada._id,
+            equipoLocal: equipoLocal,
+            equipoVisitante: equipoVisitante,
+        });
+
+        await enfrentamiento.save();
+        }
+    }
+
+    res.status(200).json({
+        ok: true,
+        mensaje: "Se han generado los enfrentamientos según las restricciones especificadas.",
+    });
 
 
 }
