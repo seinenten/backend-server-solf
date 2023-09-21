08d865047d56a5d3d6338082952cc87ac42c6a7e
Gustavoiuy <20200971@uthh.edu.mx>
2023-09-21 16:58:07 -0600

    tabla de posiciones
    traer posiciones

--- controllers/estadisticasJugadores.js
+++ controllers/estadisticasJugadores.js
@@ -1,7 +1,7 @@
 const { response } = require("express");
 
 const Enfrentamiento = require('../models/enfrentamiento');
-
+const Equipo = require('../models/equipo');
 const Estadistica = require('../models/estadisticaJugador');
 
 
@@ -71,6 +71,96 @@ const GenerarstadisticasJugadorPorEnfrentamientos = async (req, res) => {
 };
 
 
+const obtenerTablaDePosiciones = async (req, res) => {
+    try {
+        // Obtener el ID de la liga desde la solicitud (por ejemplo, desde los parámetros de la URL)
+        const { idLiga } = req.params;
+
+        // Buscar todos los enfrentamientos de la liga
+        const enfrentamientos = await Enfrentamiento.find({ liga: idLiga });
+
+        // Inicializar un objeto para almacenar las estadísticas de los equipos
+        const estadisticasEquipos = {};
+
+        // Calcular las estadísticas de los equipos a partir de los enfrentamientos
+        enfrentamientos.forEach((enfrentamiento) => {
+            const equipoLocalId = enfrentamiento.equipoLocal;
+            const equipoVisitanteId = enfrentamiento.equipoVisitante;
+
+            // Verificar si los equipos ya existen en el objeto de estadísticas
+            if (!estadisticasEquipos[equipoLocalId]) {
+                estadisticasEquipos[equipoLocalId] = { equipo: equipoLocalId, puntos: 0, goles: 0, PJ: 0, PG: 0, PE: 0, PP: 0, GF: 0, GE: 0 };
+            }
+            if (!estadisticasEquipos[equipoVisitanteId]) {
+                estadisticasEquipos[equipoVisitanteId] = { equipo: equipoVisitanteId, puntos: 0, goles: 0, PJ: 0, PG: 0, PE: 0, PP: 0, GF: 0, GE: 0 };
+            }
+
+            // Calcular los puntos y goles para cada enfrentamiento
+            const golesLocal = enfrentamiento.estadisticas.totalGolesLocal;
+            const golesVisitante = enfrentamiento.estadisticas.totalGolesVisitante;
+
+            if (golesLocal > golesVisitante) {
+                estadisticasEquipos[equipoLocalId].puntos += 3; // Ganador
+                estadisticasEquipos[equipoLocalId].PG += 1;
+                estadisticasEquipos[equipoVisitanteId].PP += 1;
+            } else if (golesLocal < golesVisitante) {
+                estadisticasEquipos[equipoVisitanteId].puntos += 3; // Ganador
+                estadisticasEquipos[equipoVisitanteId].PG += 1;
+                estadisticasEquipos[equipoLocalId].PP += 1;
+            } else {
+                estadisticasEquipos[equipoLocalId].puntos += 1; // Empate
+                estadisticasEquipos[equipoVisitanteId].puntos += 1; // Empate
+                estadisticasEquipos[equipoLocalId].PE += 1;
+                estadisticasEquipos[equipoVisitanteId].PE += 1;
+            }
+
+            estadisticasEquipos[equipoLocalId].goles += golesLocal;
+            estadisticasEquipos[equipoVisitanteId].goles += golesVisitante;
+
+            // Incrementar PJ para ambos equipos
+            estadisticasEquipos[equipoLocalId].PJ += 1;
+            estadisticasEquipos[equipoVisitanteId].PJ += 1;
+
+            // Calcular GF y GE para ambos equipos
+            estadisticasEquipos[equipoLocalId].GF += golesLocal;
+            estadisticasEquipos[equipoVisitanteId].GF += golesVisitante;
+            estadisticasEquipos[equipoLocalId].GE += golesVisitante;
+            estadisticasEquipos[equipoVisitanteId].GE += golesLocal;
+        });
+
+        // Obtener información adicional de los equipos (por ejemplo, nombres de equipos) si es necesario
+        const equipos = await Equipo.find({ _id: { $in: Object.keys(estadisticasEquipos) } });
+
+        // Construir la tabla de posiciones
+        const tablaDePosiciones = Object.values(estadisticasEquipos).map((equipo) => {
+            const equipoInfo = equipos.find((e) => e._id.toString() === equipo.equipo.toString());
+            return {
+                ...equipo,
+                nombreEquipo: equipoInfo ? equipoInfo.nombre : 'Equipo Desconocido',
+            };
+        });
+
+        // Ordenar la tabla de posiciones por puntos y goles (puedes personalizar esto según tus reglas)
+        tablaDePosiciones.sort((a, b) => {
+            if (b.puntos !== a.puntos) {
+                return b.puntos - a.puntos;
+            } else {
+                return b.goles - a.goles;
+            }
+        });
+
+        // Enviar la tabla de posiciones como respuesta JSON
+        res.status(200).json(tablaDePosiciones);
+    } catch (error) {
+        console.error('Error al obtener la tabla de posiciones:', error);
+        // Manejar el error según sea necesario
+        res.status(500).json({ error: 'Error al obtener la tabla de posiciones' });
+    }
+};
+
+
+
 module.exports = {
-    GenerarstadisticasJugadorPorEnfrentamientos
+    GenerarstadisticasJugadorPorEnfrentamientos,
+    obtenerTablaDePosiciones
 }
--- routes/estadisticasJugadores.js
+++ routes/estadisticasJugadores.js
@@ -3,11 +3,14 @@ const { Router } = require('express');
 const { validarJWT, validarADMIN_ROLE } = require('../middlewares/validar-jwt');
 
 const { 
-    GenerarstadisticasJugadorPorEnfrentamientos
+    GenerarstadisticasJugadorPorEnfrentamientos,
+    obtenerTablaDePosiciones
 } = require('../controllers/estadisticasJugadores')
 
 const router = Router();
 
 router.get('/verEstadisticas/:fecha/:id', GenerarstadisticasJugadorPorEnfrentamientos);
+
+router.get('/tablaPos/:idLiga', obtenerTablaDePosiciones);
 
 module.exports = router;
